af3fc33b8948f856a4657725472b7fa4da9ea810
Ian Langmore <ianlangmore@gmail.com>
2013-09-16 12:23:46 -0400

    UP: Made get_paths call get_paths_iter

--- declass/filefilter.py
+++ declass/filefilter.py
@@ -14,13 +14,14 @@ Contains a collection of function that clean, decode and move files around.
 """
 
 
-def get_paths(dir_name, file_type="*", relative=False):
+def get_paths(base_path, file_type="*", relative=False):
     """
-    Crawls subdirectories and returns list of paths to files that match the file_type.
+    Crawls subdirectories and returns list of paths to files that match
+    the file_type.
 
     Parameters
     ----------
-    dir_name : String
+    base_path : String
         Path to the directory that will be crawled
     file_type : String
         String to filter files with.  E.g. '*.txt'.  Note that the filenames
@@ -29,38 +30,10 @@ def get_paths(dir_name, file_type="*", relative=False):
         If True, get paths relative to dir_name
         If False, get absolute paths
     """
-    path_list = []
-    for path, subdirs, files in os.walk(dir_name, followlinks=True):
-        for name in files:
-            if fnmatch(name.lower(), file_type):
-                if relative:
-                    path = path.replace(dir_name, "")
-                    if path.startswith('/'):
-                        path = path[1:]
-                path_list.append(os.path.join(path,name))    
+    path_iter = get_paths_iter(
+        base_path, file_type=file_type, relative=relative)
 
-    return path_list
-
-
-def path_to_name(path, name_level=1):
-    """
-    Takes one path and returns a name.
-
-    Parameters
-    ----------
-    path : String
-    name_level : Integer
-        Form the name using items this far back in the path.  E.g. if
-        path = mydata/1234/3.txt and name_level == 2, then name = 1234_3
-
-    Returns
-    -------
-    name : String
-    """
-    name_plus_ext = path.split('/')[-name_level:]
-    name, ext = os.path.splitext('_'.join(name_plus_ext))
-
-    return name
+    return [path for path in path_iter]
 
 
 def get_paths_iter(base_path, file_type="*", relative=False):
@@ -90,7 +63,28 @@ def get_paths_iter(base_path, file_type="*", relative=False):
                 yield os.path.join(path, name)
 
 
-def paths_to_files(paths, mode='r'):
+def path_to_name(path, name_level=1):
+    """
+    Takes one path and returns a name.
+
+    Parameters
+    ----------
+    path : String
+    name_level : Integer
+        Form the name using items this far back in the path.  E.g. if
+        path = mydata/1234/3.txt and name_level == 2, then name = 1234_3
+
+    Returns
+    -------
+    name : String
+    """
+    name_plus_ext = path.split('/')[-name_level:]
+    name, ext = os.path.splitext('_'.join(name_plus_ext))
+
+    return name
+
+
+def paths_to_files_iter(paths, mode='r'):
     """
     Returns an iterator that opens files in path_list.
 
