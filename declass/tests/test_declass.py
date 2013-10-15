18a1863d321f5c33c32262692aeef41fd37559fe
dkrasner <krasnerdaniel@gmail.com>
2013-10-15 16:30:22 +0200

    added a topics tests

--- declass/tests/test_declass.py
+++ declass/tests/test_declass.py
@@ -5,8 +5,9 @@ from numpy.testing import assert_allclose
 from datetime import datetime
 import copy
 from collections import Counter, OrderedDict
+import pandas as pd
 
-from declass.utils import text_processors
+from declass.utils import text_processors, streamers, topic_seek
 
 
 class TestTokenizerBasic(unittest.TestCase):
@@ -22,13 +23,14 @@ class TestTokenizerBasic(unittest.TestCase):
         self.assertEqual(result, benchmark)
 
 
+@unittest.skip('skipping VW - test decrecated')
 class TestVWFormatter(unittest.TestCase):
     """
     """
     def setUp(self):
         self.Formatter = text_processors.VWFormatter
 
-    def test_format_01(self):
+    def test_for1(self):
         rec_name = 'myname'
         tokens = OrderedDict([('hello', 1), ('dude', 3)])
         result = self.Formatter().write_str(tokens, rec_name=rec_name)
@@ -40,4 +42,51 @@ class TestVWFormatter(unittest.TestCase):
         result = self.Formatter().write_dict(record_str)
         benchmark = {'hello': 1.0, 'bye': 2.0}
         self.assertEqual(result, benchmark)
+
+
+class TestTopic(unittest.TestCase):
+    def setUp(self):
+        self.Topics = topic_seek.Topics
+        self.streamer = streamers.TextFileStreamer()
+        def token_stream(tokens, doc_id=None):
+            return tokens
+        self.streamer.token_stream = token_stream
+        
+
+    def test_dictionary(self):
+        tokens1 = ['Hi', 'this', 'is', 'is', 'not', 'is', 'this']
+        tokens2 = ['one', 'two', 'one', 'three']
+        T = self.Topics()
+        T.streamer = ListStream([tokens1, tokens2])
+        T.set_dictionary(no_below=0, no_above=1)
+        result = T.dictionary.items()
+        benchmark = [(0, 'this'), (1, 'is'), (2, 'three'), (3, 'two'),
+                (4, 'Hi'), (5, 'not'), (6, 'one')]        
+        self.assertEqual(result, benchmark)
+
+    def test_get_words_docfreq(self):
+        tokens1 = ['Hi', 'this', 'is', 'is', 'not', 'is', 'this']
+        tokens2 = ['one', 'two', 'one', 'three']
+        T = self.Topics()
+        T.streamer = ListStream([tokens1, tokens2])
+        T.set_dictionary(no_below=0, no_above=1)
+        result = T.get_words_docfreq()
+        benchmark = pd.DataFrame({'tokenid': [3,2,0,6,5,1,4], 'docfreq': [1]*7}, 
+                index=['two', 'three', 'this', 'one', 'not', 'is', 'Hi'])
+        benchmark = benchmark[['tokenid', 'docfreq']]
+        self.assertEqual(result, benchmark)
+
+
+class ListStream(object):
+    def __init__(self, token_lists):
+        self.token_lists = token_lists
+
+    def token_stream(self, doc_id=None):
+        """
+        Uses 'dummy doc_id' as this is called in streamer applicatioins.
+        """
+        for t in self.token_lists:
+            yield t
+
+        
 
