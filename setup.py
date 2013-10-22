ca1becf609c24eb765bcd7b4a85abb9d7e01287e
dkrasner <dkrasner@math.columbia.edu>
2013-10-22 14:05:27 +0200

    created a setup.py file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+DISTNAME = 'declass'
+LICENSE = 'BSD'
+AUTHOR = "The DeClass Development Team"
+EMAIL = ""
+URL = "https://github.com/declassengine/declass"
+DESCRIPTION = ""
+
+setup(
+    name=DISTNAME,
+    version='0.1',
+    packages=['declass',],
+    license=LICENSE,
+    url=URL,
+    maintainer_email=EMAIL,
+    description=DESCRIPTION
+)
