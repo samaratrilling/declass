db75fe2863532a8a9da27b5b9250ba46b74c4101
Ian Langmore <ianlangmore@gmail.com>
2013-10-24 10:50:48 -0400

    ADD: cables_to_text and kiss_to_text

--- scripts/cables_to_text.py
+++ scripts/cables_to_text.py
@@ -13,7 +13,7 @@ from declass.utils.database import DBCONNECT
 # You should have your own login_file somewhere.  
 # DO NOT commit this to the (public) repository!
 login_file = os.path.join('/home/langmore/lib/declass/', 'conf', 'db_login.yml')
-RAW = os.path.join(os.getenv('DATA'), os.getenv('ME'), 'kiss-01/raw')
+RAW = os.path.join(os.getenv('DATA'), os.getenv('ME'), 'cables-01/raw')
 bodyfiles_basepath = os.path.join(RAW, 'bodyfiles')
 metafile_path = os.path.join(RAW, 'meta', 'meta.csv')
 
@@ -27,25 +27,32 @@ pwd = login_info['pwd']
 # Set up DB and retrieve all records as a dict
 # First run with "limit 10", then erase once things work
 dbCon = DBCONNECT(host_name, db_name, user_name, pwd)
-#records = dbCon.run_query('select * from Kissinger limit 10;')
-records = dbCon.run_query('select * from Kissinger;')
+fields = ['doc_nbr', 'concepts', 'date', 'msgfrom', 'office', 'origclass', 'orighandling', 'PREVCLASS', 'PREVHANDLING', 'REVIEW_AUTHORITY', 'REVIEW_DATE', 'SUBJECT', 'TAGS', 'MSGTO', 'TYPE', 'CHANNEL', 'PREVCHANNEL', 'DISP_COMMENT', 'DISP_AUTH', 'DISP_DATE', 'REVIEW_AUTH', 'REVIEW_FLAGS', 'REVIEWHISTORY', 'MSGTEXT', 'cleanFrom', 'cleanTo']
+fields = [f.lower() for f in fields]
+meta_fields = [f for f in fields if f != 'msgtext']
 
+records = dbCon.run_query(
+    'select %s '
+    'from statedeptcables '
+     'limit 5000000;' % ', '.join(fields))
 
 # Write records to disk
-meta = {'doc_id': [], 'time': [], 'names': [], 'year': [], 'subject': []}
+meta = {f: [] for f in meta_fields}
+meta['doc_id'] = []
 
 for rec in records:
-    doc_id = rec['doc_id']
+    doc_id = rec['doc_nbr'].replace(' ', '_')
     # Write the body text
     filepath = os.path.join(bodyfiles_basepath, doc_id + '.txt')
     with open(filepath, 'w') as f:
-        f.write(rec['body'])
+        f.write(rec['msgtext'])
 
-    for field in ['time', 'names', 'year', 'subject']:
+    for field in meta_fields:
         meta['doc_id'] = doc_id
         meta[field].append(rec[field])
 
 meta = pd.DataFrame(meta)
 meta.to_csv(metafile_path, sep='|', index=False)
+
 
 
